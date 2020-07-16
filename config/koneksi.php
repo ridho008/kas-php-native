1e174eb5377dfd3322a9c2e364858cd47bce04ae
ridho008 <ciuyy12@gmail.com>
2020-07-16 23:06:03 +0700

    fitur crud kas masuk

---
+++ config/koneksi.php
@@ -0,0 +1,4 @@
+<?php 
+$conn = new mysqli("localhost", "root", "", "kas_native") or die(mysqli_error($conn));
+
+?>
---
+++ index.php
@@ -0,0 +1,127 @@
+﻿<?php 
+error_reporting(0);
+require_once 'config/koneksi.php';
+
+$page = $_GET['p'];
+$aksi = $_GET['aksi'];
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Free Bootstrap Admin Template : Binary Admin</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <!-- TABLE STYLES-->
+    <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Binary admin</a> 
+            </div>
+  <div style="color: white;
+padding: 15px 50px 5px 50px;
+float: right;
+font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-danger square-btn-adjust">Logout</a> </div>
+        </nav>   
+           <!-- /. NAV TOP  -->
+                <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+				<li class="text-center">
+                    <img src="assets/img/find_user.png" class="user-image img-responsive"/>
+					</li>
+                    <li>
+                        <a  href="index.php"><i class="fa fa-dashboard fa-2x"></i> Dashboard</a>
+                    </li>
+                      <li>
+                        <a  href="?p=masuk"><i class="fa fa-desktop fa-2x"></i> Kas Masuk</a>
+                    </li>
+                    <li>
+                        <a  href="?p=keluar"><i class="fa fa-qrcode fa-2x"></i> Kas Keluar</a>
+                    </li>
+						   <li  >
+                        <a  href="?p=rekap"><i class="fa fa-bar-chart-o fa-2x"></i> Rekapitulasi Kas</a>
+                    </li>	
+                      <li  >
+                        <a  href="?p=users"><i class="fa fa-table fa-2x"></i> Management Users</a>
+                    </li>	
+                </ul>
+               
+            </div>
+            
+        </nav>  
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <!-- <h2>Blank Page</h2>   
+                        <h5>Welcome Jhon Deo , Love to see you back. </h5> -->
+                    <?php 
+                    if($page == 'masuk') {
+                        if($aksi == '') {
+                            require_once 'page/kas_masuk/masuk.php';
+                        }
+                    } else if($page == 'keluar') {
+                        if($aksi == '') {
+                            require_once 'page/kas_keluar/keluar.php';
+                        }
+                    } else if($page == 'rekap') {
+                        if($aksi == '') {
+                            require_once 'page/rekap/rekap.php';
+                        }
+                    } else if($page == 'users') {
+                        if($aksi == '') {
+                            require_once 'page/user/user.php';
+                        }
+                    }
+                    ?>
+                       
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+                 <!-- <hr /> -->
+               
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+     <!-- DATA TABLE SCRIPTS -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#dataTables-example').dataTable();
+            });
+    </script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
